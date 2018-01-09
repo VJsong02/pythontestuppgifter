49b489c72266e748d4ebec272393289d1bb4a8f1
Victor Song <VJsong02@users.noreply.github.com>
2018-01-09 18:07:58 +0100

    Initial commit

---
+++ delbart5.py
@@ -0,0 +1,9 @@
+#Two row solution
+#for i in range(200,301):
+#	print(i, ("ends in 0 or 5" if i % 5 == 0 else "doesn't end in 0 or 5"))
+
+for i in range(200,301):
+	if i % 5 == 0
+		print(i, "ends in 0 or 5")
+	else
+		print(i, "doesn't end in 0 or 5")
---
+++ division.py
@@ -0,0 +1,6 @@
+#Single row solution
+#print(int(input("First integer:")) / (int(input("Second integer:")))
+
+a = input("First integer:")
+b = input("Second integer:")
+print(int(a) / int(b))
